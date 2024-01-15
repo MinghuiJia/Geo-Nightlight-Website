3294c5d8fe6d93e9de9f42c3cb6bfd9900a58814
MinghuiJia <minghui_jia@whu.edu.cn>
2024-01-15 14:44:20 +0800

    feat: 给map增加南部非洲国家行政边界

--- src/pages/Map/Poverty/Poverty.tsx
+++ src/pages/Map/Poverty/Poverty.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { useScene } from '@antv/larkmap';
-import { PolygonLayer, Popup, MouseLocation, Control } from '@antv/l7';
+import { PolygonLayer, LineLayer, Popup, MouseLocation, Control, ILayer } from '@antv/l7';
 
 import povertyJson from '../Data/SouthAfrica/allYearsPoverty.json';
+import countriesBoundryJson from '../Data/SouthAfrica/countriesBoundry.json';
 import { SouthAfricaColors } from '../utils/legendColor';
 import { povertyLegendAdd } from '../utils/getLegendAddFunction';
 
@@ -12,7 +13,21 @@ interface Props {
 }
 
 const App: React.FC<Props> = ({ showKey }) => {
+  const [polygonLayer, setPolygonLayer] = useState<ILayer>(new PolygonLayer({}));
+
   const scene = useScene();
+
+  const boundryLayer = new LineLayer({})
+    .source(countriesBoundryJson)
+    .color('#f10c0c')
+    .active(true)
+    .size(1)
+    .style({
+      lineType: 'dash',
+      dashArray: [2, 2],
+    });
+  scene.addLayer(boundryLayer);
+
   useEffect(() => {
     const mouseLocation = new MouseLocation({
       position: 'bottomright',
@@ -29,7 +44,7 @@ const App: React.FC<Props> = ({ showKey }) => {
   }, []);
 
   useEffect(() => {
-    scene.removeAllLayer();
+    scene.removeLayer(polygonLayer);
     const layer = new PolygonLayer({})
       .source(povertyJson)
       .scale(showKey, {
@@ -42,14 +57,19 @@ const App: React.FC<Props> = ({ showKey }) => {
     layer.on('mousemove', (e) => {
       const popup = new Popup({
         offsets: [0, 0],
+        title: `${e.feature.properties.name}, ${e.feature.properties.COUNTRY}`,
         closeButton: true,
+        closeOnClick: true,
+        closeOnEsc: true,
       })
         .setLnglat(e.lngLat)
-        .setHTML(`<span>${e.feature.properties.name}: ${e.feature.properties[showKey]}%</span>`);
+        .setHTML(`<span>${e.feature.properties[showKey]}%</span>`);
       scene.addPopup(popup);
     });
 
     scene.addLayer(layer);
+
+    setPolygonLayer(layer);
   }, [showKey]);
 
   return null;
